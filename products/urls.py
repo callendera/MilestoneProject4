92783c734225bfa939113bf04bbfc7d82ca5663f
callendera <acallender@live.sscc.edu>
2020-04-22 03:36:01 +0000

    created templates for displaying product details, categories. added urls, views, and models

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, all_categories, products_by_category, products_detail, product_home
 
 urlpatterns = [
-    url(r'$', all_products, name='products'),
+    url(r'^$', all_products, name='products'),
+    url(r'^home/$', product_home, name="product_home"),
+    url(r'^category/$', all_categories, name='category'),
+    url(r'^(?P<product_slug>[-\w]+)/$', products_detail, name='products_detail'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', products_by_category, name='products_by_category')
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,75 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
+# Create your views here.
 def all_products(request):
+    products = Product.objects.filter(price="10")
+    categories = Category.objects.all()
+    return render(request, "products.html", {"products": products,
+                                             "categories": categories})
+
+
+def product_home(request):
+    products = Product.objects.all().order_by("name")
+    categories = Category.objects.all()
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+
+    index = items.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index - 5 if index >= 5 else 0
+    end_index = + 5 if index <= max_index - 5 else max_index
+    page_range = paginator.page_range[start_index:end_index]
+    return render(request, "product_home.html", {"products": products,
+                                                 "categories": categories,
+                                                 "items": items,
+                                                 "page_range": page_range, })
+
+
+def products_detail(request, product_slug):
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products}) 
+    if product_slug:
+        products = get_object_or_404(Product, slug=product_slug)
+
+    return render(request, "products_detail.html", {"products": products})
+
+
+def all_categories(request):
+    categories = Category.objects.all()
+    return render(request, "categories.html", {"categories": categories})
+
+
+def products_by_category(request, category_slug):
+    categories = Category.objects.all()
+    products = Product.objects.all().order_by("name")
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+
+    index = items.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index - 5 if index >= 5 else 0
+    end_index = + 5 if index <= max_index - 5 else max_index
+    page_range = paginator.page_range[start_index:end_index]
+    return render(request, "product_home.html", {"products": products,
+                                                 "categories": categories,
+                                                 "items": items,
+                                                 "page_range": page_range, })
