4348eef1589dcbc3c0e1747e68b8a4d123e248d8
callendera <acallender@live.sscc.edu>
2020-04-22 12:21:05 +0000

    removed certain templates from products, needed to revert back to original to refocus on design efforts

--- products/models.py
+++ products/models.py
@@ -1,38 +1,11 @@
 from django.db import models
-from django.core.urlresolvers import reverse
-
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=250, default='')
-    slug = models.SlugField(max_length=250, unique=True)
-    description = models.CharField(max_length=250, default='')
-    image = models.ImageField(upload_to='images', default='')
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-
-    def get_absolute_url(self):
-        return reverse('products_by_category', args=[self.slug])
-
-    def __str__(self):
-        return self.name
-
-
 class Product(models.Model):
-    category = models.ForeignKey(Category, null=True)
     name = models.CharField(max_length=254, default='')
-    brand = models.CharField(max_length=254, default='')
-    slug = models.SlugField(max_length=254,  unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    image2 = models.ImageField(upload_to='images', default='')
-
-    def get_absolute_url(self):
-        return reverse('products_detail', args=[self.slug])
 
     def __str__(self):
         return self.name
--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import url, include
-from .views import all_products, all_categories, products_by_category, products_detail, product_home
+from .views import all_products
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
-    url(r'^home/$', product_home, name="product_home"),
-    url(r'^category/$', all_categories, name='category'),
-    url(r'^(?P<product_slug>[-\w]+)/$', products_detail, name='products_detail'),
-    url(r'^category/(?P<category_slug>[-\w]+)/$', products_by_category, name='products_by_category')
 ]
