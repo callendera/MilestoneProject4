2ba2bd252ebd570e81dd93ad27015f12d7a5e7f5
callendera <acallender@live.sscc.edu>
2020-04-24 00:08:20 +0000

    created modal to view item details, added truncate to the products in all all products display

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=250, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
