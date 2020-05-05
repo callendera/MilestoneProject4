7d96df0a8effdf0bc3272d0fde59fe83ef4cb121
callendera <acallender@live.sscc.edu>
2020-05-05 15:53:04 +0000

    added search.html for template render in views.py searchg app

--- products/views.py
+++ products/views.py
@@ -4,4 +4,4 @@ from .models import Product
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+    return render(request, "search.html", {"products": products})
