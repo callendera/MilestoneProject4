c6d37736d4656ca0c68bee81a8853ff545ba8755
callendera <acallender@live.sscc.edu>
2020-05-05 15:56:09 +0000

    added search.html render template in wrong app, made correction

--- search/views.py
+++ search/views.py
@@ -4,4 +4,4 @@ from products.models import Product
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    return render(request, "search.html", {"products": products})
