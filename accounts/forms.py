a66c9252b7547b28dc3beb38d4b91d436719a8e8
callendera <acallender@live.sscc.edu>
2020-05-11 18:10:42 +0000

    cleaned up forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,46 +7,46 @@ from accounts.models import Customer
 
 
 class UserLoginForm(forms.Form):
-    """Form to be used to log users in"""
-    
+    """Form for logging users in"""
+
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-    
-    
+
+
 class UserRegistrationForm(UserCreationForm):
     """Form used to register a new user"""
-    
+
     password1 = forms.CharField(
-        label="Password", 
+        label="Password",
         widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label="Password Confirmation", 
+        label="Password Confirmation",
         widget=forms.PasswordInput)
-        
+
     class Meta:
         model = User
         fields = ['email', 'username', 'password1', 'password2']
-        
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
         if User.objects.filter(email=email).exclude(username=username):
             raise forms.ValidationError(u'Email address must be unique')
         return email
-        
+
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
-        
+
         if not password1 or not password2:
             raise ValidationError("Please confirm your password")
-            
+
         if password1 != password2:
             raise ValidationError("Passwords must match")
-            
-        return password2    
-     
-        
+
+        return password2
+
+
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
@@ -57,5 +57,4 @@ class CustomerForm(forms.ModelForm):
             "town_city",
             "street_address1",
             "street_address2",
-        ]
-        
+        ]  
