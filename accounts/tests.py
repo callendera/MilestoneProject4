9a911a31446452a59921d3f0de6bb282c940bc93
callendera <acallender@live.sscc.edu>
2020-05-14 02:10:02 +0000

    finished comments and organizing css

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,11 +6,13 @@ import json
 
 
 class TestloginView(TestCase):
+    # Sets up the testing to be down follwoing the setUp
     def setUp(self):
         self.client = Client()
         self.index_url = reverse('index')
 
     def test_login_view_POST(self):
+        # runs test for login view
         response = self.client.post(reverse('index'))
 
         self.assertEquals(response.status_code, 200)
